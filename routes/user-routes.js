c80cdecea8e5be1047294b16fa497e7dc9cf942b
Kevin <kevn.fu@gmail.com>
2018-05-07 10:44:43 -0700

    adding nav

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -7,24 +7,19 @@ router.get('/user', (req, res) => {
     res.redirect('/login');
     return;
   }
-
-  let UserId = req.user.id;
-  res.redirect('/user/'+ UserId);
+  res.redirect('/user/'+ req.user.id);
 });
 
 router.get('/user/:id', (req, res) => {
-  let isThisUser;
-  
-  if(!req.user) {
-    isThisUser = false;
-  } else {
-    isThisUSer = req.user.id == req.params.id;
-  }
-
   db.User.findById(req.params.id).then(user => {
-    return user.getRecipes();
+    if(user) return user.getRecipes();
+    return Promise.reject('No such user');
   }).then(recipes => {
+    // true if this is logged-in user's page
+    let isThisUser = !!req.user && (req.user.id === req.params.id);
     res.render('user-list', {recipes, isThisUser});
+  }).catch(err => {
+    res.status(404).send(err);
   });  
 });
 
